0fdf7ceee2de2eb57aa4b9b9d38b02e381ce5942
Jasper Wong <Jasper Wong>
2016-05-02 21:27:02 +0800

    add branch

--- app/src/main/java/com/example/jasper/suitcase/MainActivity.java
+++ app/src/main/java/com/example/jasper/suitcase/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.jasper.suitcase;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -58,6 +63,57 @@ public class MainActivity extends AppCompatActivity {
         if (mBluetoothAdapter == null) {
             Toast.makeText(this, "本机没有找到蓝牙硬件或驱动！", Toast.LENGTH_SHORT).show();
             finish();
+        }
+
+        if(mBluetoothAdapter.isEnabled()){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("This app needs location access");
+                    builder.setMessage("Please grant location access so this app can detect Bluetooth.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                            }
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        }
+
+        /**
+         *对授权做处理，0代表授权，-1拒绝授权
+         */
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+        String permissions[], int[] grantResults) {
+
+            switch (requestCode) {
+                case PERMISSION_REQUEST_COARSE_LOCATION: {
+                    if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                        // Log.d(TAG, "coarse location permission granted");
+                        finish();
+                    } else {
+                        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("Functionality limited");
+                        builder.setMessage("Since location access has not been granted,</span>
+                        this app will not be able to discover beacons when in the background.");
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                            }
+
+                        });
+                        builder.show();
+                    }
+                    return;
+                }
+            }
         }
 
         // 如果本地蓝牙没有开启，则开启
